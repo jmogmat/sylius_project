4d1ebd024989c57bf158ebb3aa36d31c04e344fd
matias.osorio <matias.osorio@xilon.es>
2024-08-21 14:55:49 +0200

    Initial commit of Sylius project

---
+++ src/Context/TimeBasedChannelContext.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Context;
+
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Channel\Model\ChannelInterface;
+final class TimeBasedChannelContext implements ChannelContextInterface
+
+{
+    /** @var ChannelRepositoryInterface */
+    private $channelRepository;
+
+    /**
+     * @param ChannelRepositoryInterface $channelRepository
+     */
+    public function __construct(ChannelRepositoryInterface $channelRepository)
+    {
+        $this->channelRepository = $channelRepository;
+    }
+
+
+    public function getChannel(): ChannelInterface
+    {
+        if($this->isNight()){
+           return $this->channelRepository->findOneByCode('NIGHT');
+        }
+        return $this->channelRepository->findOneBy([]);
+    }
+
+    public function isNight(): bool{
+        $currentHour = (int) (new \DateTime())->format('H');
+        return $currentHour > 19 || $currentHour < 6;
+    }
+}
